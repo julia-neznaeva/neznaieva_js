a33fa3fd9034133094257fbe2f8499bc736023ff
Juliia Neznaieva <juliia.neznaieva@vivint.com>
2024-10-31 02:45:24 +0200

    Neznaieva First homework

---
+++ task5.js
@@ -0,0 +1,23 @@
+function loop()
+{
+
+    for(let i = 1; i < 51; i++)
+    {
+        let iterationResultString;
+        iterationResultString = `${i}`;
+        if(isMultiple(i,4))
+        {
+            iterationResultString= iterationResultString+" kratne 2 & 4!";
+        }
+        else if(isMultiple(i,2)) 
+        {
+            iterationResultString = iterationResultString +" kratne 2!";
+        }
+        console.log(iterationResultString);
+    }
+}
+
+function isMultiple(shared,divider)
+{
+    return shared % divider===0;
+}
